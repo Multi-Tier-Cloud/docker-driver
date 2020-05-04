1940e41e8d5a93c3f50cdd60e265d59b508668c9
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-04 09:59:57 -0400

    Overhaul of tests, including unit and life-cycle testing   - Up to 85% coverage   - Positive and negative test cases   - Changed test image to busybox (light-weight and fast)     - There are lighter weight images but this allows running Linux       commands in case in the future we want more complex tests

--- docker_driver/docker_driver_test.go
+++ docker_driver/docker_driver_test.go
@@ -1,25 +1,133 @@
-package docker_driver
+package docker_driver_test
 
 import (
-    "fmt"
     "testing"
+
+    driver "github.com/Multi-Tier-Cloud/docker-driver/docker_driver"
 )
 
-func TestDockerDriver(test *testing.T) {
-    opt := Docker_config{
-        Image: "ubuntu",
+const (
+    testImage = "busybox"
+
+    failTestImage = "thisImageNameShouldNotExist"
+    failContID = "thisIDShouldNotExist"
+)
+
+func TestPullImage(test *testing.T) {
+    test.Run("PullImage-success", func(test *testing.T) {
+        _, err := driver.PullImage(testImage)
+        if err != nil {
+            test.Errorf("PullImage() returned:\n%v", err)
+        }
+    })
+
+    test.Run("PullImage-fail", func(test *testing.T) {
+        _, err := driver.PullImage(failTestImage)
+        if err == nil {
+            test.Errorf("PullImage() succeeded with image (%s), expected it to fail", failTestImage)
+        }
+    })
+}
+
+func TestListImages(test *testing.T) {
+    _, err := driver.ListImages()
+    if err != nil {
+        test.Errorf("ListImages() returned:\n%v", err)
+    }
+}
+
+func TestLifecycle(test *testing.T) {
+    opt := driver.Docker_config{
+        Image: "busybox",
         Port: [2]string{"4812", "4821"},
-        Cmd: []string{},
-        Memory: "500m",
+        Cmd: []string{"sleep", "300"},
+        Memory: "10m",
         Cpu: "0.5",
     }
 
-    fmt.Println(ListRunningContainers())
-    container, _ := RunContainer(opt)
-    fmt.Println(ListRunningContainers())
-    StopContainer(container)
-    DeleteContainer(container)
-    fmt.Println(ListRunningContainers())
+    containerID := ""
 
-    PullImage("hivanco/hello-world-server@sha256:3d4002fcaaa8c2a363c94ebec142447d3b04e0dc3e77954598fe97f285e0c37e")
+    // Run separate subtests for Run, Restart, Stop, and Delete
+    test.Run("RunContainer", func(test *testing.T) {
+        contID, err := driver.RunContainer(opt)
+        if err != nil || contID == "" {
+            test.Errorf("RunContainer() returned:\n%v", err)
+        }
+
+        containerID = contID
+    })
+
+
+    if containerID == "" {
+        test.Fatalf("Skipping remaining sub-tests (RunContainer() may have failed)")
+    }
+
+    test.Run("ResizeContainer", func(test *testing.T) {
+        _, err := driver.ResizeContainer(containerID, "20m")
+        if err != nil {
+            test.Errorf("ResizeContainer() returned:\n%v", err)
+        }
+    })
+
+    test.Run("RestartContainer", func(test *testing.T) {
+        _, err := driver.RestartContainer(containerID)
+        if err != nil {
+            test.Errorf("RestartContainer() returned:\n%v", err)
+        }
+    })
+
+    test.Run("StopContainer", func(test *testing.T) {
+        _, err := driver.StopContainer(containerID)
+        if err != nil {
+            test.Errorf("StopContainer() returned:\n%v", err)
+        }
+    })
+
+    test.Run("DeleteContainer", func(test *testing.T) {
+        _, err := driver.DeleteContainer(containerID)
+        if err != nil {
+            test.Errorf("DeleteContainer() returned:\n%v", err)
+        }
+    })
+
 }
+
+func TestListRunningContainers(test *testing.T) {
+    _, err := driver.ListRunningContainers()
+    if err != nil {
+        test.Errorf("ListRunningContainers() returned:\n%v", err)
+    }
+}
+
+func TestResizeContainer(test *testing.T) {
+    // Test failure case (success case covered in lifecycle test)
+    _, err := driver.ResizeContainer(failContID, "10m")
+    if err == nil {
+        test.Errorf("ResizeContainer() succeeded with container (%s), expected it to fail", failContID)
+    }
+}
+
+func TestRestartContainer(test *testing.T) {
+    // Test failure case (success case covered in lifecycle test)
+    _, err := driver.RestartContainer(failContID)
+    if err == nil {
+        test.Errorf("RestartContainer() succeeded with container (%s), expected it to fail", failContID)
+    }
+}
+
+func TestStopContainer(test *testing.T) {
+    // Test failure case (success case covered in lifecycle test)
+    _, err := driver.StopContainer(failContID)
+    if err == nil {
+        test.Errorf("StopContainer() succeeded with container (%s), expected it to fail", failContID)
+    }
+}
+
+func TestDeleteContainer(test *testing.T) {
+    // Test failure case (success case covered in lifecycle test)
+    _, err := driver.DeleteContainer(failContID)
+    if err == nil {
+        test.Errorf("DeleteContainer() succeeded with container (%s), expected it to fail", failContID)
+    }
+}
+
